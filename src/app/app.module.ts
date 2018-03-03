2d7861a125380d5c6e64e587ab961b22b59aed32
Bartosz Cynke <bartosz.cynke@gmail.com>
2018-03-03 01:30:06 +0100

    Login component with Google OAuth support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule} from "@angular/common/http";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
 
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {
+  MatButtonModule, MatCardModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatOptionModule,
+  MatSelectModule
+} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+import {AuthServiceConfig, GoogleLoginProvider, SocialLoginModule} from "angularx-social-login";
 
+let authorizationConfig = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("991607936961-c5fair5rk2mp0q05nv52e09ddjm3pp8f.apps.googleusercontent.com")
+  }
+]);
 
 @NgModule({
-  declarations: [
+  declarations : [
     AppComponent,
     LoginComponent
   ],
-  imports: [
+  imports : [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    SocialLoginModule.initialize(authorizationConfig)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers : [],
+  bootstrap : [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService, GoogleLoginProvider, SocialUser} from "angularx-social-login";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector : 'app-login',
+  templateUrl : './login.component.html',
+  styleUrls : ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  protected user : SocialUser;
+  protected serverUrl : string = 'https://todos.venturedevs.net/'
+
+  constructor(private authService : AuthService) {
+  }
 
   ngOnInit() {
+    this.authService.authState.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  loginWithGoogle() : void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  isLoggedIn() : boolean {
+    return this.user != null;
   }
 
 }
