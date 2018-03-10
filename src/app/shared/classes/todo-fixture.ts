6b23558a338b1102350aa7746fdfbf7b6762cb7e
Bartosz Cynke <bartosz.cynke@gmail.com>
2018-03-10 19:50:49 +0100

    Add missing class

---
+++ src/app/shared/classes/todo-fixture.ts
@@ -0,0 +1,82 @@
+import {ComponentFixture} from "@angular/core/testing";
+import {By} from "@angular/platform-browser";
+import {TodoList} from "../../todo-page/components/todo-list/classes/todo-list";
+import {ChangeDetectorRef, DebugElement, ElementRef} from "@angular/core";
+
+export class TodoFixture<T> {
+
+  private fixture : ComponentFixture<T>;
+  public debugElement : DebugElement;
+  public componentInstance : T;
+  public nativeElement : any;
+  public elementRef : ElementRef;
+  public changeDetectorRef : ChangeDetectorRef;
+
+  constructor(fixture : ComponentFixture<T>) {
+    this.fixture = fixture;
+    this.debugElement = fixture.debugElement;
+    this.componentInstance = fixture.componentInstance;
+    this.nativeElement = fixture.nativeElement;
+    this.elementRef = fixture.elementRef;
+    this.changeDetectorRef = fixture.changeDetectorRef;
+  }
+
+  public cssQuery(css : string) : any {
+    return this.fixture.debugElement.query(By.css(css)).nativeElement;
+  }
+
+  public clickButton(buttonSelector : string) : void {
+    this.cssQuery(buttonSelector).click();
+    this.fixture.detectChanges();
+  }
+
+  public clickElement(elementSelector : string) : void {
+    this.fixture.debugElement.query(By.css(elementSelector)).triggerEventHandler('click', null);
+    this.fixture.detectChanges();
+  }
+
+  public todoList(name : string) : TodoList {
+    let list : TodoList = new TodoList();
+    list.id = name;
+    list.name = name;
+    return list;
+  }
+
+  public setInputValue(inputSelector : string, value : string) : void {
+    let input : HTMLInputElement = this.cssQuery(inputSelector);
+    input.value = value;
+    input.dispatchEvent(new Event('input'));
+    this.fixture.detectChanges();
+  }
+
+
+  public detectChanges(checkNoChanges? : boolean) : void {
+    this.fixture.detectChanges(checkNoChanges);
+  }
+
+  checkNoChanges() : void {
+    this.fixture.checkNoChanges();
+  }
+
+  autoDetectChanges(autoDetect? : boolean) : void {
+    this.fixture.autoDetectChanges(autoDetect);
+  }
+
+  isStable() : boolean {
+    return this.fixture.isStable();
+  }
+
+  whenStable() : Promise<any> {
+    return this.fixture.whenStable();
+  }
+
+  whenRenderingDone() : Promise<any> {
+    return this.fixture.whenRenderingDone();
+  }
+
+  destroy() : void {
+    return this.fixture.destroy();
+  }
+
+
+}
